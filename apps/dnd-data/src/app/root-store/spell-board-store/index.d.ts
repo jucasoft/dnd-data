15d903706fa8c4d7eedf2eb80e47c0ed14d8a185
juca <juca@jucasoft.com>
2021-05-08 13:40:00 +0200

    start

---
+++ apps/dnd-data/src/app/root-store/spell-board-store/index.d.ts
@@ -0,0 +1,13 @@
+import * as SpellBoardStoreActions from './actions';
+import * as SpellBoardStoreSelectors from './selectors';
+import * as SpellBoardStoreState from './state';
+
+export {
+SpellBoardStoreModule
+} from './spell-board-store.module';
+
+export {
+  SpellBoardStoreActions,
+	SpellBoardStoreSelectors,
+	SpellBoardStoreState
+};
---
+++ apps/dnd-data/src/app/root-store/spell-meta-info-store/index.d.ts
@@ -0,0 +1,13 @@
+import * as SpellMetaInfoStoreActions from './actions';
+import * as SpellMetaInfoStoreSelectors from './selectors';
+import * as SpellMetaInfoStoreState from './state';
+
+export {
+SpellMetaInfoStoreModule
+} from './spell-meta-info-store.module';
+
+export {
+  SpellMetaInfoStoreActions,
+	SpellMetaInfoStoreSelectors,
+	SpellMetaInfoStoreState
+};
